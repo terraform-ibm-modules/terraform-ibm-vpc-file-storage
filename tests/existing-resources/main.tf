2223daadb608e1245d8b457287c09419c7f5a9f2
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-08-22 00:07:10 +0000

    chore(deps): update dependencies

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -191,7 +191,7 @@ resource "ibm_is_ssh_key" "ssh_key" {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "10.0.0"
+  version           = "10.0.1"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
